6cd698765a4bc075e8cfacf146f6a931922a4a8f
balianima2025v1-afk <balianima2025v1@gmail.com>
2025-10-03 23:58:24 +0800

    Update TodoList.js

--- app/components/TodoList.js
+++ app/components/TodoList.js
@@ -1,1 +1,28 @@
+import TodoItem from './TodoItem'
 
+export default function TodoList({ todos, onToggle, onDelete }) {
+  if (todos.length === 0) {
+    return (
+      <div style={{ 
+        textAlign: 'center', 
+        color: '#666',
+        padding: '40px'
+      }}>
+        Tidak ada todo. Yuk tambahkan yang baru!
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      {todos.map(todo => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          onToggle={onToggle}
+          onDelete={onDelete}
+        />
+      ))}
+    </div>
+  )
+}

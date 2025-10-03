ca66eb0e008d47eb8d9fb71dca1a032844881cf4
balianima2025v1-afk <balianima2025v1@gmail.com>
2025-10-03 23:12:49 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,1 +1,10 @@
+import TodoList from "../components/TodoList";
 
+export default function Home() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold text-center">To-Do List</h1>
+      <TodoList />
+    </div>
+  );
+}

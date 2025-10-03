7434d3291d488807c8aed8b29be338e9b58aaf44
balianima2025v1-afk <balianima2025v1@gmail.com>
2025-10-03 23:59:25 +0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,14 @@
+export const metadata = {
+  title: 'Todo App',
+  description: 'Simple Todo Application',
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body style={{ margin: 0, padding: 0, fontFamily: 'Arial, sans-serif' }}>
+        {children}
+      </body>
     </html>
-  );
+  )
 }
